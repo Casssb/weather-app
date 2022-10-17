896477c759d608d4acfa94551d75e5f0ee48e9bf
Joe <caspar0912@googlemail.com>
2022-10-17 09:25:27 +0100

    fix bug with timezone and made menu text clearer on mobiles

--- src/modules/domController.js
+++ src/modules/domController.js
@@ -64,7 +64,7 @@ const displayController = (() => {
     const low = Math.round(apiData.main.temp_min);
     const feelsLike = Math.round(apiData.main.feels_like);
     const humidity = apiData.main.humidity;
-    const timezone = apiData.timezone;
+    const timezone = apiData.timezone + (new Date().getTimezoneOffset() * 60);
     const sunrise = format(
       fromUnixTime(apiData.sys.sunrise + timezone),
       'h:mm a'
